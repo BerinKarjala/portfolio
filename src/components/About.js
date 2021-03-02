24bf99c1012b2774eda55a97ba3844ab5f52c4f5
berin karjala <berin.karjala@outlook.com>
2021-03-02 11:48:15 +0200

    refactoring of home page

--- src/components/About.js
+++ src/components/About.js
@@ -26,7 +26,7 @@ export default function About(){
     return (
         <main className="relative">
             <img src={waterDrop} alt="A drop of water in a pool creating ripples across the surface of the pool." className="absolute object-cover w-full bg-fixed" />
-            <div className="p-10 lg:pt-1 container mx-auto px-4 relative">
+            <div className="p-10 lg:pt-0 container mx-auto px-14 relative">
                 <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
                     <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name} />
                     <div className="text-lg flex flex-col justify-center">
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,10 @@ import landingImage from "../underconstruction.jpg"
 
 export default function Home() {
   return (
-    <main>
-      <div className="flex items-center justify-center h-screen"><img src={landingImage} alt="This page is under construction sorry for the inconvience" className="rounded-lg border shadow-lg p-10 w-2/4" /></div>
-      <section className="relative flex justify-center men-h-screen pt-12 lg:pt-64 px-8">
-      </section>
+    <main className="items-center justify-center h-screen p-10 lg:pt-0">
+      <div className="container mx-auto px-14 relative">
+        <img src={landingImage} alt="This page is under construction sorry for the inconvience" className="rounded-lg border shadow-lg p-10 w-2/4 " />
+      </div>
     </main>
   );
 }
