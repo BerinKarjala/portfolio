c3e56b1442c866d8deedfdfbe2eef8994be2af51
berin karjala <berin.karjala@outlook.com>
2021-03-08 06:26:41 +0200

    added embedable youtube feature to sanity studio

--- src/components/About.js
+++ src/components/About.js
@@ -24,17 +24,16 @@ export default function About(){
     }, [])
     if (!author) return <div>Loading...</div>
     return (
-        <main className="relative">
+        <main className="relative bg-gray-900">
             <img src={waterDrop} alt="A drop of water in a pool creating ripples across the surface of the pool." className="absolute object-cover w-full bg-fixed" />
             <div className="p-10 lg:pt-0 container mx-auto px-14 relative">
-                <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+                <section className="bg-white rounded-lg shadow-2xl lg:flex p-20">
                     <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name} />
                     <div className="text-lg flex flex-col justify-center">
-                        <h1 className="cursive text-6x text-green-300 mb-4">
-                            Hey there. I'm{" "}
-                            <span className="text-green-100">{author.name}</span>
+                        <h1 className="cursive text-6x text-black-300 mb-4">
+                            <span className="text-black-100">{author.name}</span>
                         </h1>
-                        <div className="prose lg:prose-xl text-white">
+                        <div className="prose lg:prose-xl text-black">
                             <BlockContent blocks={author.bio} projectId="jp1px5kh" dataset="production" />
                         </div>
                     </div>
