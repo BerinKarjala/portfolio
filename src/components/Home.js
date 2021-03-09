98b3114d7fd2663d1c77fe1f508356df913f241e
berin karjala <berin.karjala@outlook.com>
2021-03-09 02:10:21 +0200

    fixed spelling mistake on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div>
         <img src={landingImage} alt="a drop of water creating ripples in a pool" className="absolute object-cover w-full h-full" />
         <section className="relative flex justify-center min-h-screen pt-6 lg:pt-30 px-8">
-          <h1 className="text-6xl text-green-1000 font-bold cursive leading-none lg:leading-snug home-name">Tervetuola!</h1>
+          <h1 className="text-6xl text-green-1000 font-bold cursive leading-none lg:leading-snug home-name">Tervetuloa!</h1>
         </section>
       </div>
     </main>
