b162b5f122f4b0493c43f1cde24c7e366860acbe
Berin Karjala <berin.karjala@outlook.com>
2026-01-08 16:55:31 -0800

    Remove blog schema from Sanity studio

--- studio13/schemas/index.js
+++ studio13/schemas/index.js
@@ -1,8 +1,7 @@
 import author from './author'
 import blockContent from './blockContent'
 import landingPage from './landingPage'
-import post from './post'
 import project from './project'
 import youtube from './youtube'
 
-export const schemaTypes = [post, author, project, blockContent, landingPage, youtube]
+export const schemaTypes = [author, project, blockContent, landingPage, youtube]
